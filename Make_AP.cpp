c6a83ae0f584d87838e96c41f6362fa133581f2e
oustani-anas <anas.migos20@gmail.com>
2023-10-11 09:24:33 +0100

    okok

---
+++ Make_AP.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int main(){
+    ll n; cin >> n;
+    vector<ll> vect(3);
+    for (ll i = 0; i < n; i++)
+    {
+        for (ll i = 0; i < 3; i++)
+            cin >> vect[i];
+        sort(vect.begin(), vect.end());
+        if(vect[1] % vect[0] == 0 && vect[2] % vect[0] == 0)
+            cout << "yes" << endl;
+        else
+            cout << "no" << endl;
+    }
+    
+}
