1b794db7c37aa925df4d853d052e6184f424679b
oustani-anas <anas.migos20@gmail.com>
2023-11-09 13:00:28 +0100

    Cheap Travel

---
+++ CheapTravel.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#define ll long long
+#define dd double
+#define endl '\n'
+ 
+using namespace std;
+
+int main()
+{
+    ll n, m, a, b;
+    cin >> n >> m >> a >> b;
+}
