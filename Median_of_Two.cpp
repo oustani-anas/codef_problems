47aa8bdb42a25767469bec5885cffbea34524326
oustani-anas <anas.migos20@gmail.com>
2023-11-14 11:39:13 +0100

    on

--- Median_of_Two.cpp
+++ Median_of_Two.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <cmath>
 #include <iomanip>
+#include <algorithm>
 #define ll long long
 #define dd double
 #define endl '\n'
@@ -13,5 +14,29 @@ using namespace std;
 
 int main()
 {
-    
+    dd m, n, mid;
+    float res;
+    cin >> m >> n;
+    vector<ll> nums1(m), nums2(n);
+    for(ll i = 0; i < m; i++){
+        cin >> nums1[i];
+    }
+    for(ll i = 0; i < n; i++){
+        cin >> nums2[i];
+    }
+    nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+    sort(nums1.begin(), nums1.end());
+    mid = nums1.size() / 2;
+    for(auto i : nums1)
+        cout << i << " ";
+    if(nums1.size() % 2 == 0)
+    {
+        res = (nums1[mid - 1] + nums1[mid]) / 2;
+        cout << fixed << setprecision(5) << res << endl;
+    }
+    else
+    {
+        res = nums1[mid];
+        cout << fixed << setprecision(5) << res << endl;
+    }
 }
