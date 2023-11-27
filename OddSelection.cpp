d541e8165aa85a2b15f7fb01450f3d8846dbcbca
oustani-anas <anas.migos20@gmail.com>
2023-11-27 07:48:36 +0100

    okok

--- OddSelection.cpp
+++ OddSelection.cpp
@@ -1,45 +1,45 @@
 
-#include <iostream>
-#include <string>
-#include <map>
-#include <vector>
-#include <cmath>
-#include <iomanip>
-#define ll long long
-#define dd double
-#define endl '\n'
+# include <iostream>
+# include <string>
+# include <map>
+# include <vector>
+# include <cmath>
+# include <iomanip>
+# define ll long long
+# define dd double
+# define endl '\n'
  
 using namespace std;
 
-ll odd(vector<ll> &vect, ll n, ll x)
+ll n, odd, even;
+
+ll is_odd(vector<ll> &vect, ll n, ll x)
 {
-        ll unpair = 0;
-        for (ll i = 0; i < n; i++)
-        {
-            if(vect[i] % 2 != 0 ){
-                unpair++;    
-            }
-        }
-        if(unpair % 2 == 0)
-            return(1);
-        return(0);
+    
 }
+
 
 int main()
 {
-    ll n, x, t, pair;
+    ll n, x, t;
     cin >> t;
     while(t--)
     {
         cin >> n >> x;
         vector<ll> a(n);
+        odd = 0, even = 0;
         for(ll i = 0; i < n; i++)
         {
             cin >> a[i];
+            if(a[i] % 2 == 0)
+                even++;
+            else 
+                odd++;         
         }
-        if(odd(a, n, x))
-            cout << "NO" << endl;
-        else
+        // cout << "odd =  " << odd << endl;
+        if(is_odd(a, n, x))
             cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
     }
 }
