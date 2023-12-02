f339ea9782bbce5b83bdd66df578594e6edd8f34
oustani-anas <anas.migos20@gmail.com>
2023-12-02 12:55:05 +0100

    okok

---
+++ FancyFence.cpp
@@ -0,0 +1,22 @@
+
+# include <iostream>
+# include <string>
+# include <map>
+# include <vector>
+# include <cmath>
+# include <iomanip>
+# define ll long long
+# define dd double
+# define endl '\n'
+ 
+using namespace std;
+
+int main()
+{
+    ll a, t;
+    cin >> t;
+    while(t--)
+    {
+        
+    }
+}
