bdbfbc1b2d242a5b8b55009dd3d118ed5b7e6e40
oustani-anas <anas.migos20@gmail.com>
2024-05-18 07:15:38 +0100

    adding acab

---
+++ BoatsCompetition.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <utility>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+
+#define ll long long
+
+using namespace std;
+
+int main(){
+    
+}
