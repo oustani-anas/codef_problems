4ce0e5aa4c94ca009f1f8fe00500f9f09c631337
oustani-anas <anas.migos20@gmail.com>
2024-05-21 02:39:30 +0100

    okay okay 01game

--- 01GAME.cpp
+++ 01GAME.cpp
@@ -19,7 +19,33 @@
 
 using namespace std;
 
+ll one, zero;
+
+void calc_zo(string &str) {
+    for (ll i = 0; i < str.length(); i++)
+    {
+        if(str[i] == '1')
+            one++;
+        else zero++;
+    }
+}
+
 int main()
 {
-
+    ll t, td;
+    cin >> t;
+    string str;
+    while (t--)
+    {
+        one = 0, zero = 0;
+        cin >> str;
+        calc_zo(str);
+        if(one <= zero)
+            td = one;
+        else    td = zero;
+        if(td % 2)
+            cout << "DA" << endl;
+        else
+            cout << "NET" << endl;
+    }   
 }
