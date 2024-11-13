bddab6340db6ebd558a35ad814849be949b7bfc6
ANAS OUSTANI <anas@ANASs-MacBook-Air.local>
2024-11-13 03:36:49 +0100

    success of the first test case on the problem Alice chess

--- AlicesAdventuresinChess.cpp
+++ AlicesAdventuresinChess.cpp
@@ -5,25 +5,55 @@
 #define ll long long 
 using namespace std;
 
+ll n, a, b;
+string s;
+
+int reached(ll x, ll y) {
+    ll ns = 0, we = 0;
+    for(ll i = 0; i <= n; i++) {
+            i = i % n;
+            if(s[i] == 'N'){
+                y++;
+                ns++;
+            }
+            else if(s[i] == 'E') {
+                x++;
+                we++;
+            }
+            else if(s[i] == 'W') {
+                x--;
+                we--;
+            }
+            else if(s[i] == 'S') {
+                y--;
+                ns--;
+            }
+            if(i == n - 1 && ns == 0 && we == 0)
+                break;
+            //cout << s[i] << endl;
+            //cout << x << " " << y << endl;
+            if(a == x && b == y) {
+                return true;
+            }
+            if(x > a && y > b) {
+                break;
+            }
+        }
+        return false;
+}
+
+
 int main() {
     ll t; cin >> t;
-    ll n, a, b;
     ll x = 0, y = 0;
-    string s;
+
     while (t--)
     {
         cin >> n >> a >> b;
         cin >> s;
-        for(ll i = 0; i < s.size(); i++) {
-            if(s[i] == 'N')
-                y++;
-            if(s[i] == 'E')
-                x++;
-            if(s[i] == 'W')
-                x--;
-            if(s[i] == 'S')
-                y--;
-        }
-    }
-    
+        if(reached(x, y))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }    
 }
