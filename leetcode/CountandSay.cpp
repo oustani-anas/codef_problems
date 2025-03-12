7112297fe07417da1e64f2f9170b2ca5ca96f06b
oustani-anas-69 <anasoustani.highness@gmail.com>
2025-03-12 15:21:30 +0000

    creating leetcode problems folder for separate each platform

---
+++ leetcode/CountandSay.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <forward_list>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <bitset>
+#include <array>
+
+#define ll long long
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    
+}
