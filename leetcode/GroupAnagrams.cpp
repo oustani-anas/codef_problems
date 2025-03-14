efafd814e42b939a5d1c2a2ee54c08868bc3ca2a
oustani-anas-69 <anasoustani.highness@gmail.com>
2025-03-14 15:27:49 +0000

    adding the group arrangment in leetcode problem (and fixing it )

--- leetcode/GroupAnagrams.cpp
+++ leetcode/GroupAnagrams.cpp
@@ -22,19 +22,18 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mapp;
         vector<vector<string>> result;
-        int count = 0;
-        map<int, vector<string>> mapp;
-        for (auto word : strs) {
-            count = 0;
-            for(auto i : word)
-                count += i;
-            mapp[count].push_back(word);
-            // std::cout << count << endl;       
+        
+        for (auto& word : strs) {
+            string sortedWord = word;
+            sort(sortedWord.begin(), sortedWord.end()); // Sort to get a unique key for anagrams
+            mapp[sortedWord].push_back(word);
         }
-            
-        for(auto arrn : mapp)
-            result.push_back(arrn.second);
+        
+        for (auto& pair : mapp) 
+            result.push_back(pair.second);
+        
         return result;
     }
 };
