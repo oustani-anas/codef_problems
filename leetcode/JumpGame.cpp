eb5aa6dbf54b007be5bfe86d4e0d3c5d04d472c8
oustani-anas <anas.migos20@gmail.com>
2025-03-21 05:03:54 +0000

    adding the problem JumpGame from leetcode

---
+++ leetcode/JumpGame.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <forward_list>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <bitset>
+#include <array>
+
+#define ll long long
+#define endl '\n'
+
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+       // bool state = false;
+       int farthest = 0;
+        for (int i = 0; i < nums.size(); i++) 
+        {
+            if (i > farthest) return false;
+            farthest = max(farthest, i + nums[i]); 
+            if (farthest >= nums.size() - 1) return true;
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution test;
+    vector<int> arr = {2,3,1,1,4};
+    if (test.canJump(arr))
+        cout << "True" << endl;
+    else 
+        cout << "False" << endl;
+    
+
+}
