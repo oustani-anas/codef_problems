26a7b6eb7293abddca80ed07b6e3f8395abab69d
oustani-anas <anas.migos20@gmail.com>
2025-03-22 07:08:01 +0000

    adding cses problem trailing zeros

--- cses/TrailingZeros.cpp
+++ cses/TrailingZeros.cpp
@@ -6,30 +6,21 @@
 #include <cmath>
 #include <iomanip>
 #include <algorithm>
+
 #define ll long long
 #define dd double
 #define endl '\n'
+
 
 using namespace std;
 
 int main()
 {
-    //ll n; cin >> n;    
-    ll zcount = 0;
-    //cout << facto(n) << endl;
-    ll facts[10000000] = {};
-    facts[0] = 1;
-    for (ll i = 1; i < 1000000000; i++)
-    {
-        facts[i] = i * facts[i - 1];
+    ll n; cin >> n;
+    ll sum = 0;
+    while(n > 0) {
+        n = n / 5;
+        sum = sum + n;
     }
-    
-    ll num = facts[6];
-    cout << num << endl;
-    // while(num % 10 == 0) {
-    //     zcount++;
-    //     num /= 10;
-    // }
-    cout << zcount << endl;
-    return 0;
+    cout << sum << endl;
 }
