50cb78633192972f8b88d83538c242e8ceb93ea9
oustani-anas <anas.migos20@gmail.com>
2025-05-15 14:19:27 +0100

    adding the Length of the last word

---
+++ leetcode/Length_of_last_word.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <forward_list>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <sstream>
+#include <queue>
+#include <algorithm>
+#include <bitset>
+#include <array>
+
+#define ll long long
+#define endl '\n'
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        string str;
+        stringstream ss(s);
+        while(ss >> str) {}
+        // cout << str.size() << endl;
+        return str.size();
+    }
+};
+
+int main() {
+  Solution test;
+  string str = "Hello World";
+  test.lengthOfLastWord(str);  
+}
