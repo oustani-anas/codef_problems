8d554be800298f5599a40ff976a5cb6d92d54029
oustani-anas <anas.migos20@gmail.com>
2025-05-24 16:17:33 +0100

    adding the Find words contain the char

---
+++ leetcode/Find_Words_Containing_Character.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <forward_list>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <bitset>
+#include <array>
+
+#define ll long long
+#define endl '\n'
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> res;
+        for (int i = 0; i < words.size(); i++) {
+            for (auto c : words[i]) {
+                if(c == x) {
+                    res.push_back(i);
+                    break;
+                }
+            }
+        }
+        /* cout << "res: " << endl;
+        for(int i : res)
+            cout << i << " "; 
+        */
+        return res;      
+    }
+};
+
+int main() 
+{
+    Solution test;
+    vector<string> str = {"abc","bcd","aaaa","cbc"};
+    test.findWordsContaining(str, 'a');   
+}
