b14509c75a703bbae4acd05ce79347769b89f4b7
ANAS OUSTANI <youngboy@ANASs-MacBook-Air.local>
2025-06-13 15:06:05 +0100

    working on MMDP

---
+++ leetcode/Minimize_Maximum_Difference_Pairs.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <forward_list>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <bitset>
+#include <array>
+
+#define ll long long
+#define endl '\n'
+
+using namespace std;
+
+class Solution {
+public:
+    int minimizeMax(vector<int>& nums, int p) {
+            
+    }
+};
+
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    Solution test;
+    vector<int> nums = {10, 1, 2, 7, 1, 3};
+    test.minimizeMax(nums, 2);
+    
+    return 0;
+}
